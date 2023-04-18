5711549f7ce995f7809f6974dd2d69e5eb6fe791
IT21150302 <it21150302@my.sliit.lk>
2023-04-18 23:34:34 +0530

    profile-route commented

--- backend/routes/profile-routes.js
+++ backend/routes/profile-routes.js
@@ -1,35 +1,34 @@
 const express = require("express");
 const router = express.Router();
 
-//import protected-routes middlewares
-const { protectedUser} = require("../middlewares/route-authorization");
+// Import protected-routes middlewares
+const { protectedUser } = require("../middlewares/route-authorization");
 
-
-// import controllers
+// Import controllers
 const {
-    getUserDetails,
-    updateUserDetails,
-    deleteUser,
-    getProfileData,
-    getallpost,
-    createpost,
-    updatePost,
-    deletePost,
-    getCuspost
+  getUserDetails,
+  updateUserDetails,
+  deleteUser,
+  getProfileData,
+  getallpost,
+  createpost,
+  updatePost,
+  deletePost,
+  getCuspost,
 } = require("../controllers/profile-controller");
 
-// use routes
-router.route("/profile").get(protectedUser, getProfileData);
-router.route("/getprofile").get(protectedUser,getUserDetails);
-router.route("/deleteprofile/:id").delete(protectedUser,deleteUser);
-router.route("/updateprofile/:id").put(protectedUser,updateUserDetails);
+// Profile routes
+router.route("/profile").get(protectedUser, getProfileData); // Get profile data
+router.route("/getprofile").get(protectedUser, getUserDetails); // Get user details
+router.route("/deleteprofile/:id").delete(protectedUser, deleteUser); // Delete user
+router.route("/updateprofile/:id").put(protectedUser, updateUserDetails); // Update user details
 
-router.route("/getpost").get(protectedUser,getallpost);
-router.route("/getcuspost").get(getCuspost);
+// Post routes
+router.route("/getpost").get(protectedUser, getallpost); // Get all posts
+router.route("/getcuspost").get(getCuspost); // Get custom posts
 
-router.route("/createpost").post(protectedUser,createpost);
-router.route("/updatepost/:id").put(protectedUser,updatePost);
-router.route("/deletepost/:id").delete(protectedUser,deletePost);
-
+router.route("/createpost").post(protectedUser, createpost); // Create a post
+router.route("/updatepost/:id").put(protectedUser, updatePost); // Update a post
+router.route("/deletepost/:id").delete(protectedUser, deletePost); // Delete a post
 
 module.exports = router;
