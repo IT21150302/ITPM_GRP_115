175ef158f20679db5fd399623314c553a748a299
IT21150302 <it21150302@my.sliit.lk>
2023-04-18 18:33:31 +0530

    token validated

--- backend/middlewares/route-authorization.js
+++ backend/middlewares/route-authorization.js
@@ -18,3 +18,30 @@ exports.protectedUser = async (req, res, next) => {
     invalidUserResponse(res, err);
   }
 };
+
+
+const tokenValidate = (reqObj, res) => {
+  let token;
+  if (
+    reqObj.headers.authorization &&
+    reqObj.headers.authorization.startsWith("Bearer")
+  ) {
+    token = reqObj.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    res.status(401).json({ success: false, desc: "Not Authorized to Access" });
+  }
+  return token;
+};
+
+
+const noUserResponse = (res) => {
+  res.status(404).json({ success: false, desc: "No user found with this ID" });
+};
+
+const invalidUserResponse = (res, err) => {
+  res
+    .status(401)
+    .json({ success: false, desc: "Something went wrong, Frobidden-" + err });
+};
