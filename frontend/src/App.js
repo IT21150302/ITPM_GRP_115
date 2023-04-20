154eb54fbc28c7093411e00033defd395e4d9aa1
IT21150302 <it21150302@my.sliit.lk>
2023-04-20 18:02:48 +0530

    fixed issue in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Routes>
       <Route exact path="/" element={<Login />} />
       <Route exact path="/home" element={<Home />} />
+      <Route exact path="/register" element={<Register />} />
 
       </Routes>
     </BRouter>
