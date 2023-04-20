c5f1e067d9738536be45bb033c4991791498127e
IT21150302 <it21150302@my.sliit.lk>
2023-04-20 11:41:13 +0530

    login created

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as BRouter, Route, Routes } from "react-router-dom";
+import Login from './Screen/Login';
+
+
+
+function App() {
+  return (
+    <BRouter>
+      <Routes>
+      <Route exact path="/" element={<Login />} />
+
+      </Routes>
+    </BRouter>
+  );
+}
+
+export default App;
