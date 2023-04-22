3fb2004ae07ff94da3d7f4534b645a1a955e72d5
IT21150302 <it21150302@my.sliit.lk>
2023-04-22 09:06:40 +0530

    added guideline screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as BRouter, Route, Routes } from "react-router-dom";
 import Login from './Screen/Login';
-
-
+import Home from './Screen/Home';
+import Register from './Screen/Register';
+import Profile from "./Screen/Profile";
+import Guiline from "./Screen/Guideline";
 
 function App() {
   return (
@@ -12,7 +14,7 @@ function App() {
       <Route exact path="/home" element={<Home />} />
       <Route exact path="/register" element={<Register />} />
       <Route exact path="/profile" element={<Profile />} />
-
+      <Route exact path="/guidline" element={<Guiline />} />
       </Routes>
     </BRouter>
   );
---
+++ frontend/src/Screen/Guideline.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Guideline = () => {
+  return (
+    <div>Guideline</div>
+  )
+}
+
+export default Guideline
